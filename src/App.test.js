1096b44897b02ba41caaa6220f01121852a09121
André Baumgart <andre.baumgart.ext@johner-institut.de>
2020-06-23 06:42:27 +0200

    travis aws3

--- src/App.test.js
+++ src/App.test.js
@@ -13,6 +13,3 @@ test('renders learn react link', () => {
   const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
-
-http://dockerreact-env.eba-ig9kcw7n.us-east-2.elasticbeanstalk.com/
-    https://s3.console.aws.amazon.com/s3/buckets/elasticbeanstalk-us-east-2-496849308494/?region=us-east-2&tab=overview
